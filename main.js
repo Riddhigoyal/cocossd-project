713781d5f210a71c29c8b2cd20c7c8463132faae
Riddhigoyal <72336061+Riddhigoyal@users.noreply.github.com>
2021-02-21 16:49:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function desk(){
+    window.location="desk.html";
+}
+
+function state(){
+    window.location="stationary.html";
+}
+
+function bed(){
+    window.location="bed.html";
+}
+
+function dress(){
+    window.location="dress.html";
+}
+
+function kitchen(){
+    window.location="kitchen.html";
+}
+
+function nature(){
+    window.location="nature.html";
+}
